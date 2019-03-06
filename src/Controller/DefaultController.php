7626327177a6b79752b2d3b703afd361dcc11626
Sébastien Touzé <contact@sebastien-touze.fr>
2019-03-06 17:29:17 +0100

    Added submission edit capability.

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use App\Entity\Form;
 use App\Entity\Submission;
 use Doctrine\ORM\EntityManagerInterface;
-use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -33,18 +32,22 @@ class DefaultController extends AbstractController {
     }
 
     /**
-     * @Route("/submit/{id}", name="results", methods={"POST"}, requirements={"id": "\d+"})
-     * @param int                    $id
+     * @Route("/submit/{form_id}/{submission_id}", name="results", methods={"POST"},
+     *     requirements={"form_id": "\d+", "submission_id": "\d+"})
      * @param Request                $request
      *
      * @param EntityManagerInterface $em
      *
+     * @param int                    $form_id
+     * @param int|null               $submission_id
+     *
      * @return \Symfony\Component\HttpFoundation\JsonResponse
-     * @throws \Exception exception in case of error in \DateTime creation (no reason this happens in this precise situation)
+     * @throws \Exception exception in case of error in \DateTime creation (no reason this happens in this precise
+     *                    situation)
      */
-    public function results(int $id, Request $request, EntityManagerInterface $em) {
+    public function results(Request $request, EntityManagerInterface $em, int $form_id, int $submission_id = null) {
         /** @var Form $form */
-        $form = $em->getRepository('App:Form')->find($id);
+        $form = $em->getRepository('App:Form')->find($form_id);
 
         if(!$form) {
             throw $this->createNotFoundException("No form found");
@@ -52,11 +55,21 @@ class DefaultController extends AbstractController {
 
         $data = json_decode($request->getContent(), true);
 
-        $submission = new Submission();
-        $submission->setLastEdition(new \DateTime())
-            ->setRelatedForm($form)
-            ->setData($data);
-        $em->persist($submission);
+        if(is_null($submission_id)) {
+            $submission = new Submission();
+            $submission->setLastEdition(new \DateTime())
+                ->setRelatedForm($form)
+                ->setData($data);
+                $em->persist($submission);
+        } else {
+            /** @var Submission $submission */
+            $submission = $em->getRepository('App:Submission')->find($submission_id);
+            if(!$submission) {
+                throw $this->createNotFoundException("No submission found");
+            }
+            $submission->setLastEdition(new \DateTime())
+                ->setData($data);
+        }
         $em->flush();
 
         return $this->json(["status" => "success", "data" => ""]);
@@ -64,6 +77,10 @@ class DefaultController extends AbstractController {
 
     /**
      * @Route("/list_submissions/{id}", name="list_submissions", methods={"GET"}, requirements={"id": "\d+"})
+     * @param EntityManagerInterface $em
+     * @param int|null               $id
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function listSubmissions(EntityManagerInterface $em, int $id = null) {
         if(!is_null($id)) {
@@ -78,16 +95,38 @@ class DefaultController extends AbstractController {
             return $this->render('base.html.twig');
         }
 
-        return $this->render('list.html.twig', ['submissions' => $submissions]);
+        return $this->render('listSubmissions.html.twig', ['submissions' => $submissions]);
+
+    }
+
+    /**
+     * @Route("/edit_submission/{id}", name="edit_submission", methods={"GET"}, requirements={"id": "\d+"})
+     * @param EntityManagerInterface $em
+     * @param int                    $id
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function editSubmission(EntityManagerInterface $em, int $id) {
+        /** @var Submission $submission */
+        $submission = $em->getRepository('App:Submission')->find($id);
+
+        if(!$submission) {
+            throw $this->createNotFoundException("Submission not found");
+        }
+
+        return $this->render('form.html.twig', ['form' => $submission->getRelatedForm(), 'submission' => $submission]);
 
     }
 
     /**
      * @Route("/list_forms", name="list_forms", methods={"GET"})
+     * @param EntityManagerInterface $em
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function listForms(EntityManagerInterface $em) {
-            /** @var Form[] $form */
-            $forms = $em->getRepository('App:Form')->findAll();
+        /** @var Form[] $form */
+        $forms = $em->getRepository('App:Form')->findAll();
 
         return $this->render('listForms.html.twig', ['forms' => $forms]);
 
