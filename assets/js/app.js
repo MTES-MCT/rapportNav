d1084d8a08d2bfc8225cc406dbf720fdebe4ca5b
Sébastien Touzé <contact@sebastien-touze.fr>
2019-03-21 15:38:32 +0100

    [Front] Added front dependencies (Webpack encore, SurveyJS and Vue) and added template from template.gouv.fr ; starting T11.

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+/*
+ * Main JS file, calling main CSS files.
+ * This is included by base.html.twig
+ */
+
+// CSS files
+require('../css/app.css');
+require('../css/template.css');
+
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const Encore = require('@symfony/webpack-encore');
+
+//This is mostly for PHPStorm / any IDE to be able to understand this file
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
+
+Encore
+    // directory where compiled assets will be stored
+    .setOutputPath('public/build/')
+    // public path used by the web server to access the output path
+    .setPublicPath('/build')
+    // only needed for CDN's or sub-directory deploy
+    //.setManifestKeyPrefix('build/')
+
+    /*
+     * ENTRY CONFIG
+     */
+    //Common for most pages
+    .addEntry('app', './assets/js/app.js')
+
+    // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
+    .splitEntryChunks()
+
+    // only one instance of modules even for multiple require in js files (ex. : required in aps.js and page.js)
+    .enableSingleRuntimeChunk()
+
+    /*
+     * FEATURE CONFIG
+     *
+     * Enable & configure other features below. For a full
+     * list of features, see:
+     * https://symfony.com/doc/current/frontend.html#adding-more-features
+     */
+    .cleanupOutputBeforeBuild()
+    .enableBuildNotifications()
+    .enableSourceMaps(!Encore.isProduction())
+    // enables hashed filenames (e.g. app.abc123.css)
+    .enableVersioning(Encore.isProduction())
+
+    // uncomment if you use TypeScript
+    //.enableTypeScriptLoader()
+
+    // uncomment if you're having problems with a jQuery plugin
+    //.autoProvidejQuery()
+
+    // uncomment if you use API Platform Admin (composer req api-admin)
+    //.enableReactPreset()
+    //.addEntry('admin', './assets/js/admin.js')
+;
+
+module.exports = Encore.getWebpackConfig();
