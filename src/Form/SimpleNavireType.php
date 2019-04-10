32af68a241674abd22f3325d352b6068def808cf
Sébastien Touzé <contact@sebastien-touze.fr>
2019-04-10 09:30:49 +0200

    Immatriculation now fill all the fields for a Navire, console information display in case of error.

--- src/Form/SimpleNavireType.php
+++ src/Form/SimpleNavireType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Navire;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -14,7 +15,9 @@ class SimpleNavireType extends AbstractType {
         $builder
             ->add('immatriculation_fr', TextType::class, ['attr' => ['class' => "immatriculation_fr"]])
             ->add('nom', TextType::class)
-            ->add('longueurHorsTout', NumberType::class);
+            ->add('longueurHorsTout', NumberType::class)
+            ->add('idNavFloteur', HiddenType::class)
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver) {
