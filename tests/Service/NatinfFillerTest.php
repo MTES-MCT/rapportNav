f352f59e29fb9ccd7055f12e39d9f4e2f8b2508f
Sébastien Touzé <sebastien.touze@developpement-durable.gouv.fr>
2019-08-07 17:34:02 +0200

    Improved Service/NatinfFiller Tests.

--- tests/Service/NatinfFillerTest.php
+++ tests/Service/NatinfFillerTest.php
@@ -4,12 +4,20 @@ namespace App\Tests\Service;
 
 use App\Entity\Natinf;
 use App\Service\NatinfFiller;
+use Doctrine\Common\Persistence\ObjectRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use PHPUnit\Framework\TestCase;
 
 class NatinfFillerTest extends TestCase {
     public function testFromArray() {
+        $fakeRepo = $this->createMock(ObjectRepository::class);
+        $fakeRepo->expects($this->any())
+            ->method('findOneBy')
+            ->willReturn(null);
         $fakeEm = $this->createMock(EntityManagerInterface::class);
+        $fakeEm->expects($this->any())
+            ->method('getRepository')
+            ->willReturn($fakeRepo);
         $natinfFiller = new NatinfFiller($fakeEm, "http://localhost:8090/api/");
 
         $data = ["32208", "00000"];
