f79b6743347875f75cf03ba765b8c6efeb22d730
Sébastien Touzé <sebastien.touze@developpement-durable.gouv.fr>
2019-12-18 13:27:41 +0100

    Add edit support for timeDivision.

--- assets/js/timeDivision.js
+++ assets/js/timeDivision.js
@@ -1,26 +1,34 @@
 import Vue from "vue";
 import timeDivision from "./timeDivision.vue";
-import vSelect from "vue-select";
+import $ from "jquery";
 
 Vue.component('time-division', timeDivision);
 
-new Vue({
-    el: "#time-division",
-    component: { timeDivision },
-    data: {
-        controleMer: null,
-        controleTerre: null,
-        controleAireProtegeeMer: null,
-        controleAireProtegeeTerre: null,
-        visiteSecurite: null,
-        surveillanceManifestationMer: null,
-        surveillanceManifestationTerre: null,
-        surveillanceDpmMer: null,
-        surveillanceDpmTerre: null,
-    },
-    methods: {
-        test: function (data) {
-            console.log(data);
-        }
-    }
+$(document).ready(function () {
+
+    new Vue({
+        el: "#time-division",
+        component: {timeDivision},
+        data: {
+            controleMer: null,
+            controleTerre: null,
+            controleAireProtegeeMer: null,
+            controleAireProtegeeTerre: null,
+            visiteSecurite: null,
+            surveillanceManifestationMer: null,
+            surveillanceManifestationTerre: null,
+            surveillanceDpmMer: null,
+            surveillanceDpmTerre: null,
+        },
+        mounted: function () {
+            const rapport = $('#rapport-data').data('content') || {};
+            if (1 > Object.keys(rapport).length) {
+                return
+            }
+            // this.data = rapport.timeDivision;
+            for (let [index, time] of Object.entries(rapport.timeDivision)) {
+                    this[index] = time;
+            }
+        },
+    });
 });
