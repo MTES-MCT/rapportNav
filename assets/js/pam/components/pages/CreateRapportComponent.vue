0ae60d5695d7125d39129d08a945c2a269107206
aleckvincent <7Clutch973!0>
2021-11-16 18:36:16 +0100

    [WIP] add content create rapport

---
+++ assets/js/pam/components/pages/CreateRapportComponent.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class=" fr-mt-9w">
+    <div class="fr-grid-row">
+      <div class="listMenuSide fr-mr-5w">
+        <nav class="fr-ml-14w fr-text--sm fr-mt-n1w">
+          <ul class="list-unstyled">
+            <div class="fr-grid-row">
+              <div class="fr-col-2"><span class="fr-fi-arrow-right-line fr-fi" aria-hidden="true"></span></div>
+              <div class="fr-col-10"><li class="nav-list active text-icon--left"> Informations générales</li></div>
+            </div>
+            <div class="fr-grid-row">
+              <div class="fr-col-2"></div>
+              <div class="fr-col-10"><li class="nav-list">Activités du navire</li></div>
+            </div>
+            <div class="fr-grid-row">
+              <div class="fr-col-2"></div>
+              <div class="fr-col-10"><li class="nav-list">Contrôles opérationnels</li></div>
+            </div>
+            <div class="fr-grid-row">
+              <div class="fr-col-2"></div>
+              <div class="fr-col-10"><li class="nav-list">Indicateurs de missions</li></div>
+            </div>
+
+          </ul>
+        </nav>
+      </div>
+      <div class=" mainContent">
+        <h5 class="text-blue-france text-800">Informations générales</h5>
+        <div class="box-shadow-card">
+          <h6>Dates de la marée</h6>
+            <div class="form-inline">
+                <label class="fr-label" for="start-date">
+                  De
+                </label>
+                <div class="fr-input-wrap fr-fi-calendar-line">
+                  <input class="form-valid form-custom form-date" type="date" id="start-date" name="text-input-calendar">
+                </div>
+                <div class="fr-input-wrap">
+                  <input class="form-valid form-custom form-time" type="time" id="start-time" name="text-input-calendar">
+                </div>
+
+              <label class="fr-label" for="end-date">
+                à
+              </label>
+              <div class="fr-input-wrap fr-fi-calendar-line">
+                <input class="form-date" type="date" id="end-date" name="text-input-calendar">
+              </div>
+              <div class="fr-input-wrap">
+                <input class="form-time" type="time" id="end-time" name="text-input-calendar">
+              </div>
+            </div>
+        </div>
+      </div>
+      <div class="listMenuSide"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CreateRapportComponent"
+};
+</script>
--- assets/js/pam/pam.js
+++ assets/js/pam/pam.js
@@ -1,7 +1,13 @@
 import Vue from "vue";
+import CreateRapportComponent from "./components/pages/CreateRapportComponent";
 import HeaderComponent from "./components/HeaderComponent";
 
 new Vue({
     components: { HeaderComponent },
     template: '<HeaderComponent />',
 }).$mount('#headerComponent ')
+
+new Vue({
+    components: { CreateRapportComponent },
+    template: '<CreateRapportComponent />',
+}).$mount('#createRapportComponent ')
