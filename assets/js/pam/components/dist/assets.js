59303df2cc071cd50fcd8c299824e5f25595c3aa
aleckvincent <7Clutch973!0>
2021-11-17 12:57:15 +0100

    add remixicon

--- assets/js/pam/components/dist/assets.js
+++ assets/js/pam/components/dist/assets.js
@@ -3,3 +3,4 @@ function requireAll(r) { r.keys().forEach(r); }
 requireAll(require.context('../../../../../node_modules/@gouvfr/dsfr/dist/css/', true, /\.css$/));
 
 import '../../../../css/pam/base.css';
+import 'remixicon/fonts/remixicon.css'
