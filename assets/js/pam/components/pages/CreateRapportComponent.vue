e0db5395a584da40050c827487e135ceb4aef300
aleckvincent <7Clutch973!0>
2021-11-18 09:42:25 +0100

    [WIP Create rapport] add history sidebar

--- assets/js/pam/components/pages/CreateRapportComponent.vue
+++ assets/js/pam/components/pages/CreateRapportComponent.vue
@@ -376,14 +376,104 @@
             </ul>
           </div>
         </div>
-
-
-
       </div>
 
 
 
-      <div class="listMenuSide"></div>
+      <div class="listMenuSide">
+        <h6><i class="ri-history-line fr-mt-4w fr-mr-1v"></i> Historique</h6>
+        <div class="fr-container--fluid-auto">
+          <div class="history-group">
+            <span class="history-date">Aujourd'hui</span>
+            <div class="history-body">
+              <div class="fr-grid-row">
+                <div class="fr-col">
+                  <span class="history-time">19:12</span>
+                </div>
+              </div>
+              <div class="fr-grid-row">
+                <div class="fr-col-2">
+                  <img src="http://placebeard.it/155/150?26" class="fr-responsive-img" alt="">
+                </div>
+                <div class="fr-col-8 fr-ml-2v">
+                  <p class="history-main-content">
+                    <span class="text-bold">Romain Crampon</span> à réalisé 2 modifications.
+                    <span class="history-see-more-details">Masquer les détails</span>
+                  </p>
+                  <div class="history-details">
+                    <hr>
+                    Ajout de "David Vincent" aux membres de l'équipe
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="history-body">
+              <div class="fr-grid-row">
+                <div class="fr-col">
+                  <span class="history-time">13:12</span>
+                </div>
+              </div>
+              <div class="fr-grid-row">
+                <div class="fr-col-2">
+                  <img src="http://placebeard.it/155/150?26" class="fr-responsive-img" alt="">
+                </div>
+                <div class="fr-col-8 fr-ml-2v">
+                  <p class="history-main-content">
+                    <span class="text-bold">Romain Crampon</span> à réalisé 2 modifications.
+                    <span class="history-see-more-details">Voir les details</span>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="history-group">
+            <span class="history-date">jeudi 16 septembre</span>
+            <div class="history-body">
+              <div class="fr-grid-row">
+                <div class="fr-col">
+                  <span class="history-time">14:12</span>
+                </div>
+              </div>
+              <div class="fr-grid-row">
+                <div class="fr-col-2">
+                  <img src="http://placebeard.it/155/150?26" class="fr-responsive-img" alt="">
+                </div>
+                <div class="fr-col-8 fr-ml-2v">
+                  <p class="history-main-content">
+                    <span class="text-bold">Romain Crampon</span> à réalisé 2 modifications.
+                    <span class="history-see-more-details">Masquer les détails</span>
+                  </p>
+                  <div class="history-details">
+                    <hr>
+                    Ajout de "David Vincent" aux membres de l'équipe
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="history-body">
+              <div class="fr-grid-row">
+                <div class="fr-col">
+                  <span class="history-time">11:12</span>
+                </div>
+              </div>
+              <div class="fr-grid-row">
+                <div class="fr-col-2">
+                  <img src="http://placebeard.it/155/150?26" class="fr-responsive-img" alt="">
+                </div>
+                <div class="fr-col-8 fr-ml-2v">
+                  <p class="history-main-content">
+                    <span class="text-bold">Romain Crampon</span> à réalisé 2 modifications.
+                    <span class="history-see-more-details">Voir les details</span>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
