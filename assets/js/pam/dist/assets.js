66e7a5ba9a3a25092b05543b8b1b08c79bec4f54
aleckvincent <7Clutch973!0>
2021-11-26 14:15:28 +0100

    override grid css

--- assets/js/pam/dist/assets.js
+++ assets/js/pam/dist/assets.js
@@ -34,3 +34,4 @@ import '@gouvfr/dsfr/dist/component/modal/modal.module';
 //import '@gouvfr/dsfr/dist/js/accordions.nomodule.min.js';
 import '../../../css/pam/table-controle.css'
 import '../../../css/pam/table-indicateur.css'
+import '../../../css/override/_grid_dsfr.css'
