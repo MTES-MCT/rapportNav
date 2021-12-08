96762a15b061a7c9d2faa579477cdf5ab7b24c59
Aleck Vincent <aleck.vincent@gmail.com>
2021-12-08 17:37:32 +0100

    refactoring code linked to mission display

--- assets/js/pam/components/IndicateurMissionComponent.vue
+++ assets/js/pam/components/IndicateurMissionComponent.vue
@@ -3,16 +3,12 @@
   <div class="heading-custom heading-custom-space-between">
     <h5 class="text-blue-france text-800">Indicateurs de mission</h5>
   </div>
-  <AccordionIndicateurMissionComponent
-      v-if="controleNavirePro !== null"
-      :title="controleNavirePro.type.label"
-      :indicateurs="controleNavirePro.indicateurs"
-  ></AccordionIndicateurMissionComponent>
-
-  <!--<AccordionIndicateurMissionComponent
-      title="Contrôle en mer des navires de pêche plaisance"
-      :types="missions.types[1]"
-  ></AccordionIndicateurMissionComponent>-->
+  <div v-for="mission in missions">
+    <AccordionIndicateurMissionComponent
+        :title="mission.type.label"
+        :indicateurs="mission.indicateurs"
+    ></AccordionIndicateurMissionComponent>
+  </div>
 </div>
 </template>
 
@@ -26,14 +22,6 @@ export default {
       type: Array,
       default: null
     }
-  },
-  mounted() {
-
-    this.missions.forEach((mission, index) => {
-      if(mission.type.id === 1 ) {
-        this.controleNavirePro = mission;
-      }
-    })
   },
   data() {
     return {
