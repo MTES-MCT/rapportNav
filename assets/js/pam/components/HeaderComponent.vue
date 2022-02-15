0bbbdc4a22c5542c2326062ab25f26cc1cba5a0e
Aleck Vincent <aleck.vincent@gmail.com>
2022-02-15 14:57:14 +0100

    test url without concatenation

--- assets/js/pam/components/HeaderComponent.vue
+++ assets/js/pam/components/HeaderComponent.vue
@@ -44,12 +44,12 @@
                     <div class="fr-collapse fr-menu" id="menu-776">
                       <ul class="fr-menu__list">
                         <li>
-                          <a class="fr-nav__link fr-btn--icon-left fr-fi-download-line" :href="sanitizeUrl('/api/pam/export/rapport/' + this.numReport + (this.draft ? '?draft' : ''))" target="_self">
+                          <a class="fr-nav__link fr-btn--icon-left fr-fi-download-line" :href="sanitizeUrl('/api/pam/export/rapport')" target="_self">
                             Télécharger le rapport de patrouille (.docx)</a>
                         </li>
                         <li>
                           <a class="fr-nav__link fr-btn--icon-left fr-fi-download-line" 
-                            :href="sanitizeUrl('/api/pam/export/indicateurs/' + this.numReport + (this.draft ? '?draft' : ''))" target="_self">
+                            href="#" target="_self">
                             Télécharger les indicateurs de mission (.xlsx)
                           </a>
                         </li>
