e1f535521c19518d0d83808adcba4d8fbb48654a
Aleck Vincent <aleck.vincent@gmail.com>
2022-02-21 16:10:24 +0100

    ajout bouton telechargement en page d'accueil

---
+++ assets/js/pam/components/dropdown/HomeDownloadComponent.vue
@@ -0,0 +1,53 @@
+<template>
+  <div >
+    <button class="fr-btn fr-btn--secondary" @click="hidden = !hidden" ref="downloadDropdown" >
+      <i class="ri-download-2-line fr-mr-2v" aria-hidden="true" />
+     Télécharger
+    </button>
+    <div class="navigation-download"   :id="'navigation-' + rapport.id" v-if="!hidden" v-click-outside="hide" >
+      <ul>
+        <li @click="exportRapport">
+          Télécharger le rapport de patrouille (.docx)
+        </li>
+        <li @click="exportIndicateurs">
+          Télécharger les indicateurs de mission (.xlsx)
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import {sanitizeUrl} from "@braintree/sanitize-url";
+
+export default {
+  name: "HomeDownloadComponent",
+  props: {
+    rapport: Object
+  },
+  mounted() {
+  },
+  methods: {
+    hide(event) {
+      if(!this.$refs.downloadDropdown.contains(event.target)) {
+        this.hidden = true;
+      }
+    },
+    exportIndicateurs() {
+      window.location.href = sanitizeUrl('/api/pam/export/indicateurs/' + this.rapport.id + (this.rapport.type === 'brouillon' ? '?draft' : ''));
+    },
+    exportRapport() {
+      window.location.href = sanitizeUrl('/api/pam/export/rapport/' + this.rapport.id + (this.rapport.type === 'brouillon' ? '?draft' : ''));
+    }
+  },
+  data() {
+    return {
+      hidden: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
