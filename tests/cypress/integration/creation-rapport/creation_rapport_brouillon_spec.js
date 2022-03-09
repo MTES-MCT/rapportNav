1666d824d1cfc3c1c26f05f11a1268451436beef
Aleck Vincent <aleck.vincent@gmail.com>
2022-03-09 17:34:08 +0100

    decoupage test brouillon en plusieurs tests selon fonctionnalités

--- tests/cypress/integration/creation-rapport/creation_rapport_brouillon_spec.js
+++ tests/cypress/integration/creation-rapport/creation_rapport_brouillon_spec.js
@@ -5,31 +5,23 @@ describe('Creation d\'un brouillon', () => {
         cy.visit('/pam');
     });
 
-    it('Je dois arriver sur la page de création de rapport et sauvegarder un brouillon', () => {
-        cy.viewport('macbook-15')
+    it('Je dois arriver sur la page de création de rapport et remplir les dates du rapport', () => {
 
-        // Accès à la page de création de rapport
         cy.get('#home_header_creer_un_rapport_btn').click();
+        cy.remplirDatesRapport();
+        cy.get('#datetimePicker_start')
+            .find('.selected-date')
+            .should('have.css', 'background-color')
+            .and('eq', 'rgb(242, 249, 245)');
 
-        // Remplissage des dates du rapport
-        cy.get('#datetimePicker_start').click();
-        cy.get('#datetimePicker_start__day__1').click();
-        cy.get('#datetimePicker_start').click();
-        cy.get('#datetimePicker_start_timepicker__group_hour').find('input').clear();
-        cy.get('#datetimePicker_start_timepicker__group_hour').find('input').type('15');
-        cy.get('#datetimePicker_start_timepicker__group_minute').find('input').clear();
-        cy.get('#datetimePicker_start_timepicker__group_minute').find('input').type('15');
+        cy.get('#datetimePicker_end')
+            .find('.selected-date')
+            .should('have.css', 'background-color')
+            .and('eq', 'rgb(242, 249, 245)');
+    })
 
-        cy.get('#datetimePicker_end').click();
-        cy.get('#datetimePicker_end__day__12').click();
-        cy.get('#datetimePicker_end').click();
-        cy.get('#datetimePicker_end_timepicker__group_hour').find('input').clear();
-        cy.get('#datetimePicker_end_timepicker__group_hour').find('input').type('12');
-        cy.get('#datetimePicker_end_timepicker__group_minute').find('input').clear();
-        cy.get('#datetimePicker_end_timepicker__group_minute').find('input').type('15');
-
-
-        // Agent des agents
+    it('Je dois pouvoir ajouter un agent', () => {
+        cy.get('#home_header_creer_un_rapport_btn').click();
         cy.get('#input_ajouter_membres').type('Mar', {force: true});
         cy.get('#ajout_suggestion_1').click({force: true});
         cy.get('.members-list').contains('Marceline Desbordes-Valmore');
@@ -40,83 +32,50 @@ describe('Creation d\'un brouillon', () => {
 
 
         cy.get('.members-list').contains('John Doe');
+    })
 
-        // Check des missions à réaliser
+
+    it('Je dois pouvoir cocher une mission réalisé', () => {
+        cy.get('#home_header_creer_un_rapport_btn').click();
         cy.get('#checkboxes-1').check({force: true});
         cy.get('.main-task-active').should('exist');
+    })
 
-        //Activités du navire
+    it('Je dois pouvoir remplir les activités du navire ', () => {
+        cy.get('#home_header_creer_un_rapport_btn').click();
         cy.get('#nb_jours_mer').type('15');
         cy.get('#nav_eff').type('3');
         cy.get('#mouillage').type('4');
         cy.get('#maintenance').type('5').type('{enter}');
-
-
         cy.get('.dayInSea').contains('15 jours en mer');
         cy.get('.missionTrackTime').contains('12 heures de mission');
+    });
 
-
-        //Controles opérationnels
-        remplissageControles(1); // Remplissage controles en mer de navires de pêche professionnelle
+    it('Je dois pouvoir remplir les controles opérationnel ', () => {
+        cy.get('#home_header_creer_un_rapport_btn').click();
+        cy.remplirControles(1);
 
         cy.get('.btn-add-controle').click();
         cy.get('#controle_mer_plaisance_pro').click(); //Ajout controles en mer de plaisance professionnelle
 
-        remplissageControles(2); // Remplissage en controles en mer de navires de plaisance professionnelle
+        cy.remplirControles(2); // Remplissage en controles en mer de navires de plaisance professionnelle + assertions
+    });
 
-
-        //Indicateurs de missions
+    it('Je dois pouvoir remplir les indicateurs de missions ', () => {
+        cy.get('#home_header_creer_un_rapport_btn').click();
         const $tdFillableIndicateurs = cy.get('#accordion-indicateurs-1').find('.td-fillable');
-
         $tdFillableIndicateurs.each(($el, index) => {
-            cy.wrap($el).type('3')
+            cy.wrap($el).type('3', {force: true})
         });
+    });
 
-
+    it('Je dois pouvoir sauvegarder un rapport en brouillon', () => {
+        cy.get('#home_header_creer_un_rapport_btn').click();
+        cy.remplirDatesRapport();
         cy.get('#header_enregistrer_brouillon_btn').click();
         cy.get('.Vue-Toastification__toast--info').should('be.visible');
         cy.get('.Vue-Toastification__toast--success').should('be.visible');
     })
 })
 
-/**
- * Remplissage formulaire contrôles opérationnels
- *
- * @param index id de la categorie de Controle
- */
-function remplissageControles(index) {
-    for(let i = 2; i <= 8; i++) {
-        if(i <= 6) {
-            cy.get('#add-btn_' + index).click({force: true});
 
-            cy.get('#add-column_dropdown_' + index)
-                .find('#add-column_list_ajouter_pv')
-                .find('#add-column_list_ajouter_pv__list')
-                .find('#add-column_list_ajouter_pv__item_' + i)
-                .click({force: true});
-        } else {
-            cy.get('#add-btn_' + index).click({force: true});
-            cy.get('#add-column_dropdown_' + index)
-                .find('#add-column_list_ajouter_pv__item_' + i)
-                .click({force: true});
-        }
-    }
-
-    cy.get('#controle_op_table_' + index).find('#nb_pv_equipement_securite').should('be.visible');
-    cy.get('#controle_op_table_' + index).find('#nb_pv_titre_nav').should('be.visible');
-    cy.get('#controle_op_table_' + index).find('#nb_pv_police').should('be.visible');
-    cy.get('#controle_op_table_' + index).find('#nb_pv_env_pollution').should('be.visible');
-    cy.get('#controle_op_table_' + index).find('#nb_autre_pv').should('be.visible');
-    cy.get('#controle_op_table_' + index).find('#nb_nav_deroute').should('be.visible');
-    cy.get('#controle_op_table_' + index).find('#nb_nav_interroge').should('be.visible');
-
-    cy.get('#controle_op_table_' + index).find('#nb_navire_controle').clear({force: true}).type('16');
-    cy.get('#controle_op_table_' + index).find('#nb_pv_peche_sanitaire').clear({force: true}).type('23');
-    cy.get('#controle_op_table_' + index).find('#nb_pv_equipement_securite').clear({force: true}).type('3');
-    cy.get('#controle_op_table_' + index).find('#nb_pv_titre_nav').clear({force: true}).type('7');
-    cy.get('#controle_op_table_' + index).find('#nb_pv_police').clear({force: true}).type('9');
-    cy.get('#controle_op_table_' + index).find('#nb_pv_env_pollution').clear({force: true}).type('11');
-    cy.get('#controle_op_table_' + index).find('#nb_autre_pv').clear({force: true}).type('14');
-    cy.get('#controle_op_table_' + index).find('#nb_nav_deroute').clear({force: true}).type('4');
-    cy.get('#controle_op_table_' + index).find('#nb_nav_interroge').clear({force: true}).type('2');
-}
