2ff8c7ed1d37c3fac7d7db576b01661e0799b268
Aleck Vincent <aleck.vincent@gmail.com>
2022-03-09 17:33:40 +0100

    add commands

---
+++ tests/cypress/support/commands/login.js
@@ -0,0 +1,20 @@
+Cypress.Commands.add('loginAsUser', () => {
+    cy.request('/login')
+        .its('body')
+        .then((body) => {
+            const $html = Cypress.$(body);
+            const csrf = $html.find('input[name=_csrf_token]').val();
+
+            cy.request({
+                method: 'POST',
+                url: '/login_check',
+                form: true,
+                body: {
+                    _username: 'alfred.de-musset',
+                    _password: '1234',
+                    _csrf_token: csrf,
+                    _submit: 'connexion'
+                }
+            });
+        });
+});
