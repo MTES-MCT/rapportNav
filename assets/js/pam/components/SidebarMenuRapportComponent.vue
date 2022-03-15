16f775ae40b93dd30a4789f718a18958705fcff1
Aleck Vincent <aleck.vincent@gmail.com>
2022-03-15 11:13:19 +0100

    add automatic active on scroll

--- assets/js/pam/components/SidebarMenuRapportComponent.vue
+++ assets/js/pam/components/SidebarMenuRapportComponent.vue
@@ -18,13 +18,42 @@ export default {
       const className = this.$el.querySelector('.' + hash);
       this.setActive(className);
     }
-
+    this.automaticActiveScroll();
   },
   methods: {
     setActive(target) {
       const currentActiveClass = this.$el.querySelector('.active');
       currentActiveClass.classList.remove('active');
       target.classList.add('active');
+    },
+    automaticActiveScroll() {
+      let informationGeneralY = this.getOffset( document.getElementById('generalInformation'));
+      let shipActivityY = this.getOffset( document.getElementById('shipActivity'));
+      let controleOpY = this.getOffset( document.getElementById('controle'));
+      let indicateurY = this.getOffset( document.getElementById('indicateur'));
+
+      window.onscroll = () => {
+        if(window.scrollY >= informationGeneralY) {
+          this.setActive(this.$el.querySelector('.generalInformation'))
+        }
+        if(window.scrollY >= shipActivityY) {
+          this.setActive(this.$el.querySelector('.shipActivity'))
+        }
+        if(window.scrollY >= controleOpY) {
+          this.setActive(this.$el.querySelector('.controle'))
+        }
+        if(window.scrollY >= indicateurY) {
+          this.setActive(this.$el.querySelector('.indicateur'))
+        }
+      }
+    },
+    getOffset( el ) {
+      let _y = 0;
+      while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
+        _y += el.offsetTop - el.scrollTop;
+        el = el.offsetParent;
+      }
+      return (_y - 160); // 160 = margin
     }
   }
 }
