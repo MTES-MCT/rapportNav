a711ed6773744ace5aabe0885c3c54b5e44324b8
Aleck Vincent <aleck.vincent@gmail.com>
2022-11-23 10:03:12 +0100

    ajout categorie de deroutement dans admin

---
+++ src/Admin/CategorieDeroutementAdmin.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class CategorieDeroutementAdmin extends AbstractAdmin {
+
+    protected function configureFormFields(FormMapper $formMapper): void {
+        $formMapper
+            ->add('nom', TextType::class, ['required' => true]);
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper): void {
+        $datagridMapper
+            ->add('nom');
+    }
+
+    protected function configureListFields(ListMapper $listMapper): void {
+        $listMapper
+            ->addIdentifier('nom');
+    }
+
+}
