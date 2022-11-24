f64a8db68468ac5dc1bc40c5ee59a0f300d2bcc6
Aleck Vincent <aleck.vincent@gmail.com>
2022-11-24 15:59:11 +0100

    ajout back office contact

--- assets/js/pam/components/FooterComponent.vue
+++ assets/js/pam/components/FooterComponent.vue
@@ -1,13 +1,36 @@
 <template>
   <footer>
     <a href="#">Documentation</a>
-    <a href="#">Contact</a>
+    <a :href="'mailto:' + mailto" v-if="mailto">Contact</a>
   </footer>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  name: "FooterComponent"
+  name: "FooterComponent",
+  mounted() {
+    let mailto = null;
+    axios.get('/api/contacts').then((response) => {
+      let contacts = response.data;
+      contacts.forEach((contact) => {
+        if(contact.isRecipient) {
+          mailto = contact.email;
+        }
+        if(contact.isCopy) {
+          mailto = mailto + '?cc=' + contact.email;
+        }
+      })
+
+      this.mailto = mailto;
+    })
+  },
+  data() {
+    return {
+      mailto: null
+    }
+  }
 }
 </script>
 
