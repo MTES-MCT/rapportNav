011894a1edb553e255e7170049101e790850c8fc
Aleck Vincent <aleck.vincent@gmail.com>
2022-11-24 14:31:27 +0100

    ajout vue component Footer

--- assets/js/pam/App.vue
+++ assets/js/pam/App.vue
@@ -1,10 +1,16 @@
 <template>
-  <router-view></router-view>
+  <div>
+    <router-view></router-view>
+
+    <FooterComponent/>
+  </div>
 </template>
 
 <script>
+import FooterComponent from "./components/FooterComponent";
 export default {
-  name: "App"
+  name: "App",
+  components: {FooterComponent}
 }
 </script>
 
---
+++ assets/js/pam/components/FooterComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer>
+    <a href="#">Documentation</a>
+    <a href="#">Contact</a>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "FooterComponent"
+}
+</script>
+
+<style scoped>
+
+</style>
