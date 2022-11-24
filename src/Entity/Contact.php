06b39834b00cc9fef3a7c3cff44c7b8b113a4df7
Aleck Vincent <aleck.vincent@gmail.com>
2022-11-24 15:59:32 +0100

    ajout back office contact

---
+++ src/Entity/Contact.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ContactRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ContactRepository::class)
+ */
+class Contact
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=64)
+     */
+    private $firstName;
+
+    /**
+     * @ORM\Column(type="string", length=64)
+     */
+    private $lastName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isRecipient;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $isCopy;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getIsRecipient(): ?bool
+    {
+        return $this->isRecipient;
+    }
+
+    public function setIsRecipient(bool $isRecipient): self
+    {
+        $this->isRecipient = $isRecipient;
+
+        return $this;
+    }
+
+    public function getIsCopy(): ?bool
+    {
+        return $this->isCopy;
+    }
+
+    public function setIsCopy(?bool $isCopy): self
+    {
+        $this->isCopy = $isCopy;
+
+        return $this;
+    }
+}
