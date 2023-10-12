a084ff1bed725203493f90f537874dec9e80f82b
aleckvincent <aleck.vincent@gmail.com>
2023-10-12 10:12:05 +0200

    ajout navpro dans admin

---
+++ src/Admin/NavProUnitaireAdmin.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+use App\Entity\PAM\PamRapport;
+
+final class NavProUnitaireAdmin extends AbstractAdmin
+{
+
+    protected function configureDatagridFilters(DatagridMapper $filter): void
+    {
+        $filter
+            ->add('id')
+            ->add('date')
+            ->add('pvEmis')
+            ->add('brouillon')
+            ;
+    }
+
+    protected function configureListFields(ListMapper $list): void
+    {
+        $list
+            ->add('id')
+            ->add('pvEmis')
+            ->add('date')
+            ->add('brouillon')
+            ->add(ListMapper::NAME_ACTIONS, null, [
+                'actions' => [
+                    'show' => [],
+                    'edit' => [],
+                    'delete' => [],
+                ],
+            ]);
+    }
+
+    protected function configureFormFields(FormMapper $form): void
+    {
+        $form
+            ->add('pvEmis')
+            ->add('date')
+            ->add('brouillon')
+            ;
+    }
+
+    protected function configureShowFields(ShowMapper $show): void
+    {
+        $show
+            ->add('id')
+            ->add('date')
+            ->add('pvEmis')
+            ->add('brouillon')
+            ;
+    }
+
+}
